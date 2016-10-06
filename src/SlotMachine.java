d227fabecd469e9bd18f90edb07ff4fc900329c4
Willem H. Sherwood <wsherwood@ccsnh.edu>
2016-10-06 15:29:14 -0400

    Simple changes for SlotMachine to make it less mutable

--- src/SlotMachine.java
+++ src/SlotMachine.java
@@ -1,12 +1,13 @@
-
 /**
  * Created by Willem Sherwood on 10/5/2016.
  */
+
 class SlotMachine {
     private Slot[] slots;
     private final int _DEFAULTNUMBERSLOTS = 3;
-    private int _MaxSlots = 0;
+    private final MaxSlots;
     private int currentBet = 0;
+    private final Random _RANDOMGENERATOR;
 
     /**
      * Default Constructor the sets _MaxSlots to 3
@@ -15,8 +16,16 @@ class SlotMachine {
         buildSlots( _DEFAULTNUMBERSLOTS );
     }
 
-    public SlotMachine(int numOfSlots ){
+    public SlotMachine(int numOfSlots){
         buildSlots( numOfSlots );
+    }
+
+    //Test Constructor
+    public SlotMachine(bool test){
+        if( test ){
+            buildSlots( _DEFAULTNUMBERSLOTS );
+            
+        }
     }
 
     public void spin(){
@@ -35,13 +44,20 @@ class SlotMachine {
             addCoins(coins);
         }
     }
-
+    
+    //--------------Private Methods-------------
     private void addCoins(int coins){
         currentBet += coins;
     }
 
     private void removeCoins(int coins){
-        currentBet -= coins;
+        if( currentBet - coins < 0 ){
+            currentBet = 0;
+        }
+        else{
+            currentBet -= coins;
+        }
+	   
     }
 
     //-----------Slot Machine Factory------------
@@ -49,15 +65,14 @@ class SlotMachine {
     private void buildSlots( int numSlots ){
         if( _MaxSlots != 0 ){
             System.err.println(this.toString() + "_MaxSlots has already been built!!!");
-            return;                             //Break out of the function
+            return;                              //Break out of the function
         }
 
         _MaxSlots = numSlots;
         slots = new Slot[numSlots];
 
-        for( int i = 0; i < numSlots; i++)
-        {
-
+        for( int i = 0; i < numSlots; i++){
+            
         }
 
     }
